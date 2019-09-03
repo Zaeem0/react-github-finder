0158c3f335db283ced6e09b32d31dfed72e89814
zaeem0 <zaeem.q97@gmail.com>
2019-09-03 21:56:22 +0100

    mock users list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/navbar";
 import "./App.css";
+import Users from "./components/users/Users";
 
 class App extends Component {
   name = () => "John Doe";
@@ -8,6 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar title="Github Finder" />
+        <div className="container">
+          <Users />
+        </div>
         <h1>Hello {this.name()}</h1>
       </div>
     );
