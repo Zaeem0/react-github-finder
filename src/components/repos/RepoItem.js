95c2a1ff13c777038fa7a1ad59848298ff290527
zaeem0 <zaeem.q97@gmail.com>
2019-09-06 23:13:07 +0100

    repo icon

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -5,8 +5,9 @@ const RepoItem = ({ repo }) => {
   return (
     <div className="card">
       <h3>
-        <a href={repo.html_url}>{repo.name}</a>
+        <i class="fas fa-folder" /> <a href={repo.html_url}>{repo.name}</a>
       </h3>
+      <p>{repo.description}</p>
     </div>
   );
 };
