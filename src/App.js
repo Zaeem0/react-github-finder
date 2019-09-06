530d8233691a94193b44162e218497a120427f3b
zaeem0 <zaeem.q97@gmail.com>
2019-09-06 22:09:57 +0100

    user page

--- src/App.js
+++ src/App.js
@@ -6,24 +6,25 @@ import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search";
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
 
+  //Load users on page load
   async componentDidMount() {
     this.setState({
       loading: true
     });
 
     let url = `
-    https://api.github.com/users?
-    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-    client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}&
+    https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}&
     `;
     const res = await axios.get(url);
 
@@ -33,6 +34,7 @@ class App extends Component {
     });
   }
 
+  //Search users by name and bio
   searchUsers = async text => {
     this.setState({
       loading: true,
@@ -40,9 +42,7 @@ class App extends Component {
     });
 
     let url = `
-    https://api.github.com/search/users?q=${text}&
-    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}&
+    https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}&
     `;
 
     const res = await axios.get(url);
@@ -52,14 +52,35 @@ class App extends Component {
       loading: false
     });
   };
+
+  //Single user result
+  getUser = async username => {
+    console.log("getting user");
+    this.setState({ loading: true });
+
+    let url = `
+    https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}&
+    `;
+
+    const res = await axios.get(url);
+
+    this.setState({
+      user: res.data,
+      loading: false
+    });
+  };
+
+  //Reset search results
   clearUsers = () => this.setState({ users: [], loading: false });
+
+  //Search validation alert
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
     setTimeout(() => this.setState({ alert: null }), 3000);
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, user } = this.state;
     return (
       <Router>
         <div className="App">
@@ -83,6 +104,18 @@ class App extends Component {
                 )}
               />
               <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
