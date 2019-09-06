26984c076492d56c9c737e3fa8cf8322f796a00d
zaeem0 <zaeem.q97@gmail.com>
2019-09-06 20:05:12 +0100

    search bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Navbar from "./components/layout/Navbar";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 
 class App extends Component {
   state = {
     users: [],
     loading: false
   };
+
   async componentDidMount() {
     this.setState({
       loading: true
@@ -26,11 +28,32 @@ class App extends Component {
       loading: false
     });
   }
+
+  searchUsers = async text => {
+    this.setState({
+      loading: true
+    });
+
+    let url = `
+    https://api.github.com/search/users?q=${text}&
+    client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}&
+    `;
+
+    const res = await axios.get(url);
+
+    this.setState({
+      users: res.data.items,
+      loading: false
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
