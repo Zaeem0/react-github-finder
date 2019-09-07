a851471853311079ec666bf51255111b641546c6
zaeem0 <zaeem.q97@gmail.com>
2019-09-07 15:12:23 +0100

    context setup

---
+++ src/context/types.js
@@ -0,0 +1,14 @@
+// USERS
+export const SEARCH_USERS = "SEARCH_USERS";
+export const GET_USERS = "GET_USERS";
+export const CLEAR_USERS = "CLEAR_USERS";
+
+// REPOS
+export const GET_REPOS = "GET_REPOS";
+
+// ALERT
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
+
+// LOADING
+export const SET_LOADING = "SET_LOADING";
